8a99525f26d983cc5447e96ac6168797d2a9ad7c
Michael Angerman <stormasm@gmail.com>
2018-07-19 16:00:35 -0700

    starting to work on the text for the home page

--- m3/src/components/Home.js
+++ m3/src/components/Home.js
@@ -1,9 +1,71 @@
 import React from "react";
+import { withStyles } from "@material-ui/core/styles";
+import Typography from "@material-ui/core/Typography";
 
-const Home = () => (
-  <div>
+/*
+const styles = {
+  root: {
+    minHeight: 50,
+    width: '100%',
+  }
+};
+*/
+
+/*
+const styles = {
+  root: {
+    display: 'flex',
+    justifyContent: 'center',
+    flexWrap: 'wrap',
+  },
+};
+
+
+const styles = {
+  root: {
+    flex: '1 1 100%',
+    justifyContent: 'center',
+  },
+};
+*/
+const styles = theme => ({
+  root: theme.mixins.gutters({
+    paddingTop: 20,
+    flex: '1 1 100%',
+    maxWidth: '100%',
+    margin: '0 auto',
+  }),
+});
+
+
+
+function Home(props) {
+  const { classes } = props;
+  return (
+    <div className={classes.root}>
     <h2>Home</h2>
-  </div>
-);
+    <p>
+      This is a test for the next 60 seconds,...  This is only a test.
+      I like this test.
+      Alot...
+    </p>
+    <Typography>
+      This is another test
+    </Typography>
+    <Typography>
+      hola
+    </Typography>
 
-export default Home;
+    <Typography gutterBottom variant="title" component="p">
+      <a href={"https://github.com/stormasm/ghw-doc/blob/master/ghw.md"} target="_blank">More Info</a>
+    </Typography>
+    <Typography component="p">rock</Typography>
+    <Typography component="p" className={classes.location} gutterBottom>
+      <b>Life is Good !</b>
+    </Typography>
+
+
+  </div>
+  );
+}
+export default withStyles(styles)(Home);
