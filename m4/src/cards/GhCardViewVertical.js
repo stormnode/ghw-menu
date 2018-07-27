fce35e91e17f4313b582717e82e909767995b291
Michael Angerman <stormasm@gmail.com>
2018-07-27 16:07:59 -0700

    initial commit of GhCardViewVertical

---
+++ m4/src/cards/GhCardViewVertical.js
@@ -0,0 +1,55 @@
+import React from "react";
+import PropTypes from "prop-types";
+import { withStyles } from "@material-ui/core/styles";
+import GridList from "@material-ui/core/GridList";
+import GridListTile from "@material-ui/core/GridListTile";
+import GhCard from "./GhCard";
+import lightGreen from "@material-ui/core/colors/lightGreen";
+
+const styles = theme => ({
+  root: {
+    display: "flex",
+    flexWrap: "wrap",
+    justifyContent: "space-around",
+    overflow: "hidden",
+    height: 250,
+    backgroundColor: theme.palette.background.paper
+  },
+  gridList: {
+    width: 600,
+    backgroundColor: lightGreen[100]
+  },
+  gridListSingleLine: {
+    flexWrap: "nowrap",
+    // Promote the list into his own layer on Chrome. This cost memory but helps keeping high FPS.
+    transform: "translateZ(0)",
+    backgroundColor: lightGreen[100]
+  },
+  subheader: {
+    width: "100%"
+  }
+});
+
+function GhCardViewVertical(props) {
+  const { classes, tileData } = props;
+
+  return (
+    <div>
+      <div className={classes.root}>
+        <GridList cellHeight={250} className={classes.gridList} cols={3}>
+          {tileData.map(tile => (
+            <GridListTile key={tile.avatar} cols={tile.cols || 1}>
+              <GhCard tile={tile} />
+            </GridListTile>
+          ))}
+        </GridList>
+      </div>
+    </div>
+  );
+}
+
+GhCardViewVertical.propTypes = {
+  tileData: PropTypes.array.isRequired
+};
+
+export default withStyles(styles)(GhCardViewVertical);
--- m4/src/views/View1.js
+++ m4/src/views/View1.js
@@ -1,51 +1,15 @@
 import React from "react";
 import PropTypes from "prop-types";
-import { withStyles } from "@material-ui/core/styles";
-import GridList from "@material-ui/core/GridList";
-import GridListTile from "@material-ui/core/GridListTile";
-import GhCard from "./../cards/GhCard";
-import lightGreen from "@material-ui/core/colors/lightGreen";
+import GhCardViewVertical from "./../cards/GhCardViewVertical";
 import RepoView from "./../components/RepoView";
 
-const styles = theme => ({
-  root: {
-    display: "flex",
-    flexWrap: "wrap",
-    justifyContent: "space-around",
-    overflow: "hidden",
-    height: 250,
-    backgroundColor: theme.palette.background.paper
-  },
-  gridList: {
-    width: 600,
-    backgroundColor: lightGreen[100]
-  },
-  gridListSingleLine: {
-    flexWrap: "nowrap",
-    // Promote the list into his own layer on Chrome. This cost memory but helps keeping high FPS.
-    transform: "translateZ(0)",
-    backgroundColor: lightGreen[100]
-  },
-  subheader: {
-    width: "100%"
-  }
-});
-
 function View1(props) {
-  const { classes, tileData, repoName, viewName } = props;
+  const { tileData, repoName, viewName } = props;
 
   return (
     <div>
       <RepoView repoName={repoName} viewName={viewName} />
-      <div className={classes.root}>
-        <GridList cellHeight={250} className={classes.gridList} cols={3}>
-          {tileData.map(tile => (
-            <GridListTile key={tile.avatar} cols={tile.cols || 1}>
-              <GhCard tile={tile} />
-            </GridListTile>
-          ))}
-        </GridList>
-      </div>
+      <GhCardViewVertical tileData={tileData} />
     </div>
   );
 }
@@ -56,4 +20,4 @@ View1.propTypes = {
   viewName: PropTypes.string.isRequired
 };
 
-export default withStyles(styles)(View1);
+export default View1;
