0a56565b283236f722d50306e2dfb1d055425b61
Michael Angerman <stormasm@gmail.com>
2018-07-27 16:14:05 -0700

    initial commit of GhCardViewHorizontal

---
+++ m4/src/cards/GhCardViewHorizontal.js
@@ -0,0 +1,59 @@
+import React from "react";
+import PropTypes from "prop-types";
+import { withStyles } from "@material-ui/core/styles";
+import GridList from "@material-ui/core/GridList";
+import GridListTile from "@material-ui/core/GridListTile";
+import GhCard from "./../cards/GhCard";
+import lightGreen from "@material-ui/core/colors/lightGreen";
+
+const styles = theme => ({
+  root: {
+    display: "flex",
+    flexWrap: "wrap",
+    justifyContent: "space-around",
+    overflow: "hidden",
+    height: 250,
+    backgroundColor: theme.palette.background.paper
+  },
+  gridList: {
+    width: 600,
+    backgroundColor: lightGreen[100]
+  },
+  gridListSingleLine: {
+    flexWrap: "nowrap",
+    // Promote the list into his own layer on Chrome. This cost memory but helps keeping high FPS.
+    transform: "translateZ(0)",
+    backgroundColor: lightGreen[100]
+  },
+  subheader: {
+    width: "100%"
+  }
+});
+
+function GhCardViewHorizontal(props) {
+  const { classes, tileData } = props;
+
+  return (
+    <div>
+      <div className={classes.root}>
+        <GridList
+          cellHeight={250}
+          className={classes.gridListSingleLine}
+          cols={6}
+        >
+          {tileData.map(tile => (
+            <GridListTile key={tile.avatar} cols={tile.cols || 1}>
+              <GhCard tile={tile} />
+            </GridListTile>
+          ))}
+        </GridList>
+      </div>
+    </div>
+  );
+}
+
+GhCardViewHorizontal.propTypes = {
+  tileData: PropTypes.array.isRequired
+};
+
+export default withStyles(styles)(GhCardViewHorizontal);
